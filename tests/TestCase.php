ebfcf33e9226c28df5d1cce0548351d36a2c0222
oiseaubleuc <147993651+oiseaubleuc@users.noreply.github.com>
2025-08-13 19:30:23 +0200

    ci

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,18 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication, InteractsWithViews;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (method_exists($this, 'withoutVite')) {
+            $this->withoutVite();
+        }
+    }
 }
